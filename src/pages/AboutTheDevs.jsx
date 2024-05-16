2f1f8817e674b56574982d03ca82c7fc2157720c
Iris Beckham <irisdancer3@gmail.com>
2024-05-16 10:09:17 -0700

    about us component and page

---
+++ src/pages/AboutTheDevs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutUs from "../components/AboutUs"
+
+const AboutTheDevs = () => {
+    return (
+        <div className="bg-amber-50">
+            <div className="py-12 text-center text-4xl font-bold">Meet the Team</div>
+            <hr className="mx-10 border-2 border-black" />
+            <AboutUs />
+        </div>
+    );
+};
+
+export default AboutTheDevs;
