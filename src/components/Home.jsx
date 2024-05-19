ee47d51f2917a9f8771b13c9d56c2e61314a75ce
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-05-19 13:58:58 -0400

    landing page built

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div className="bg-red-500 py-10"> Your Are Home</div>;
+  return (
+    <>
+      <div className="">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute top-0 left-0 min-w-full min-h-full object-cover"
+          //   className="absolute inset-0 w-full h-full object-cover z-0"
+        >
+          <source
+            src="https://res.cloudinary.com/dvmczcg3f/video/upload/v1716066866/landfill4_pdgcsm.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <div className="relative inset-y-56 text-white">
+          <div className="flex items-center justify-center text-white">
+            <div className="text-center border-4 p-5 rounded-2xl bg-black">
+              <h1 className="text-4xl md:text-6xl font-bold mb-4">
+                No Waste With,
+              </h1>
+              <h2 className="text-4xl md:text-6xl font-bold mb-8">
+                Smart Waste!!!
+              </h2>
+              <p className="text-lg md:text-xl mb-8">
+                Conscious waste management for NYC residents WHO CARE!
+              </p>
+              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300">
+                Learn More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
