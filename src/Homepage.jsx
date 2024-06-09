1151a7d6b796027e82c9262830473a51ceceb862
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-09 12:54:17 -0400

    added the Sections component to the Homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,17 +1,12 @@
 import { useState, useEffect } from "react";
-import { getNYTArticles } from "./components/fetch";
-import Section from "./components/Section";
+import Sections from "./components/Sections";
 import { Link } from "react-router-dom";
 
 function Homepage ({ sections }) {
-
     return (
         <div>
             <h1>HomePage</h1>
-            {sections.length >0 && sections.map((section) =>
-                <section key={section}>
-                    <Link to={`/sections/${sections.indexOf(section)}`} className="section-link">{section}</Link>
-                </section>)}
+            <Sections sections={sections} />
         </div>
     )
 }
